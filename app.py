16569447e9c1928e785084e1184622c7bc27ff8c
EdwBit <73597471+edwbit@users.noreply.github.com>
2024-10-04 16:49:52 +0800

    update

--- app.py
+++ app.py
@@ -3,3 +3,5 @@ from typing import Generator
 from groq import groq
 
 st.set_page_config(page_icon="💬", layout="centered", page_title="Groq Chat")
+
+st.subheader("Groq Chat Streamlit App", divider="rainbow", anchor="false")
